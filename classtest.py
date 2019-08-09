8bb8f3877288d61ba1282413af2f4393c563af03
kuangklq <kuangklq@126.com>
2019-08-09 13:05:41 +0800

    shiyan07

---
+++ classtest.py
@@ -0,0 +1,12 @@
+class UserData:
+    def __init__(self, ID, Name):
+        self.ID = ID
+        self.Name = Name
+    def __repr__(self):
+        return 'ID:{} Name:{}'.format(self.ID, self.Name)
+
+if __name__ == '__main__':
+    user1 = UserData(101, 'Jack')
+    user2 = UserData(102, 'Louplus')
+    print(user1)
+    print(user2)
