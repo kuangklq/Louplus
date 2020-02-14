ecfe558a2a111f035415b9e1a61933c2255a3060
kuangklq <kuangklq@126.com>
2020-02-15 04:17:25 +0800

    none

--- extest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-num = input("Enter a number:")
-try:
-    new_num = int(num)
-    print('INT:{}'.format(new_num))
-except ValueError:
-    print('ERROR: abc')
--- jump7.py
+++
@@ -1,8 +0,0 @@
-i = 0
-while i < 100:
-	i += 1
-	if (i % 7 == 0) or ((i - 7) % 10 == 0) or (70 <= i <= 79):
-		continue
-	else:
-		print(i)
-
